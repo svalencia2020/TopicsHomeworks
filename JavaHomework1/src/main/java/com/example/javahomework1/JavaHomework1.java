c25f1fc4869ecc9554eb2abaeb122be0a8e11aa5
svalencia2020 <svalencia2020@ihs.immaculateheart.org>
2019-09-03 11:39:03 -0700

    This is my homework1

---
+++ JavaHomework1/src/main/java/com/example/javahomework1/JavaHomework1.java
@@ -0,0 +1,21 @@
+package com.example.javahomework1;
+
+public class JavaHomework1 {
+    public static void main(String[] args) {
+        String name = "Sam";
+        String age = "seventeen";
+        boolean sameLength = name == age;
+        System.out.println("Hi" + "my name is " + name + " and my age is seventeen");
+        System.out.println("In uppercase, my name is " + name.toUpperCase());
+        System.out.println("In lowercase, my name is " + name.toLowerCase());
+        System.out.println("My name is " + name.length() + " characters long");
+        System.out.println("My age is " + age.length() + " characters long");
+        System.out.println("Does my name equal my age? " + sameLength);
+        System.out.println("My name is still " + name);
+
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/topicshomeworks/Homework1.java
+++ app/src/main/java/com/example/topicshomeworks/Homework1.java
@@ -2,6 +2,9 @@ package com.example.topicshomeworks;
 
 public class Homework1 {
     public static void main(String[] args) {
-        System.out.println("HI");
+        String name = "Sam";
+        int age = 'seventeen';
+        System.out.println("Hi" + "my name is " + name + "and my age is seventeen");
+
     }
 }
