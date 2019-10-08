b85e8bbb9d3d7ba08383e7528740f33f1a9b5baf
svalencia2020 <svalencia2020@ihs.immaculateheart.org>
2019-10-07 18:06:31 -0700

    Homework 6!

--- JavaHomework1/src/main/java/com/example/javahomework1/Homework6.java
+++ JavaHomework1/src/main/java/com/example/javahomework1/Homework6.java
@@ -1,5 +1,26 @@
 package com.example.javahomework1;
 
 public class Homework6 {
-    System.out.println("Hi"); 
+    int gradeOne = 0;
+    int gradeTwo = 0;
+
+    public static void gradeCalc (int gradeOne, int gradeTwo){
+
+        if(gradeOne>90 && gradeTwo>85) {
+            System.out.println("Grade is an A.");
+        }
+
+        if(gradeOne<85 && gradeTwo>90) {
+            System.out.println("Grade is an B.");
+        }
+
+        if(gradeOne>75 && gradeTwo>75) {
+            System.out.println("Grade is an C.");
+        }
+    }
+
+    public static void main(String[] args) {
+System.out.println(gradeCalc(78,98));
+
+    }
 }
